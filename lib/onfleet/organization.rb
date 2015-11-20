823af2b19f58ed6f1e51336d431e25016b90a00b
Eric Kreutzer <eric@lu.gg>
2015-11-20 11:37:44 -0800

    Org fix

--- lib/onfleet/organization.rb
+++ lib/onfleet/organization.rb
@@ -3,11 +3,7 @@ module Onfleet
     # http://docs.onfleet.com/docs/organizations#get-details
     # http://docs.onfleet.com/docs/organizations#get-delegatee-details
     def self.retrieve(id = nil)
-      if id
-        path = "organizations/#{id}"
-      else
-        path = "organization"
-      end
+      path = id ? "organizations/#{id}" : "organization"
       response = get path
       new(response)
     rescue Nestful::ResourceNotFound
